70d66a6e637e4ed873c40efb5137d720a11e7ddd
Emil Jonsson <emijonss@kth.se>
2020-01-31 17:46:26 +0100

    Add GIT API call to set commit check status #6
    Works  on specific test case but will have to be generalized
    Also needs work on authentication

--- src/main/java/CI.java
+++ src/main/java/CI.java
@@ -7,12 +7,23 @@ import java.io.IOException;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.api.ContentResponse;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
+import org.eclipse.jetty.client.util.StringContentProvider;
+import org.eclipse.jetty.http.HttpHeader;
+import org.eclipse.jetty.client.api.AuthenticationStore;
+import org.eclipse.jetty.client.util.BasicAuthentication;
+import java.net.URI;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
 import org.eclipse.jgit.api.*;
+
+
+
 
 /**
  Skeleton of a ContinuousIntegrationServer which acts as webhook
@@ -39,7 +50,7 @@ public class CI extends AbstractHandler
         // 2nd compile the code
 
         gitClone();
-        
+
 
         response.getWriter().println("CI job done");
     }
@@ -55,14 +66,49 @@ public class CI extends AbstractHandler
       }
     }
 
+    private static void setStatus(String url) throws Exception {
+      String GitUserName = "";
+      String GitPassWord = "";
+      //String url = "https://api.github.com/repos/Eimutt/DD2480-G20-Assignment2/statuses/0cb960cedcef7f84c38ccc9d804a503f8bde0592";
+
+      SslContextFactory.Client sslContextFactory = new SslContextFactory.Client();
+      HttpClient httpClient = new HttpClient(sslContextFactory);
+
+      httpClient.setFollowRedirects(false);
+
+      String jsonStr = "{\"state\": \"error\", \"description\": \"It test!\", \"target_url\": \"http://localhost\"}";
+
+      try {
+        httpClient.start();
+
+        AuthenticationStore auth = httpClient.getAuthenticationStore();
+        URI uri = URI.create("url");
+        auth.addAuthenticationResult(new BasicAuthentication.BasicResult(uri, GitUserName, GitPassWord));
+
+        ContentResponse response = httpClient.POST("url")
+                .header(HttpHeader.CONTENT_TYPE, "application/json; charset=utf-8")
+                .content(new StringContentProvider(jsonStr))
+                .send();
+
+        System.out.println(response.getContentAsString());
+
+        return;
+      } finally {
+          httpClient.stop();
+      }
+
+    }
+
 
     // used to start the CI server in command line
     public static void main(String[] args) throws Exception
     {
-
+        setStatus("{'state': 'success', 'description': 'It works!', 'target_url': 'http://localhost'}");
+        /*
         Server server = new Server(8020);
         server.setHandler(new CI());
         server.start();
         server.join();
+        */
     }
 }
