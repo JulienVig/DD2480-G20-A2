88d67575add4673ac9a8ea44364aecd568a788c2
JulienVig <33122365+JulienVig@users.noreply.github.com>
2020-02-03 15:51:40 +0100

    Configure Travis #12 (#13)
    Configure the Travis file .travis.yml in order to perform automated tests.
    Needed to change the java version used by the travis build.
    Closes #12

--- src/main/java/GitUtil.java
+++ src/main/java/GitUtil.java
@@ -37,7 +37,8 @@ public class GitUtil {
          connection.newBuild().forTasks("build")
          .setStandardOutput(System.out).run();
       } catch(Exception e){
-        //System.out.println("************************ Error -> "+e);
+
+        System.out.println("************************ Error -> "+e);
         success = false;
       }
       finally {
