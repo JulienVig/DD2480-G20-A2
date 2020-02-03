e54edfe415c6fcb43ef3cc3949662277c1b183c0
JulienVig <33122365+JulienVig@users.noreply.github.com>
2020-02-03 15:47:04 +0100

    Compile and Test assessment #7 #10 (#11)
    * Feat build and test 
    
    The CI server now builds the cloned repository. So it compiles it and runs the unit tests.
    If the build is unsuccesful the boolean success is set to false.
    Github webhook timeout after 10 sec. Implements multi-threading
    to send the response before building the project.
    GitUtil class contains static functions to maniupulate the git repo.
    New tests for gitClone() and deleteRepo().
    Tests that the build is successful for the branch assessment
    and fails for the branch assess_error.
    Closes #7 
    Closes #10

--- src/main/java/CI.java
+++ src/main/java/CI.java
@@ -2,17 +2,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 
-import java.io.IOException;
-
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.*;
 
 import org.eclipse.jgit.api.*;
+import org.gradle.tooling.*;
+import java.util.concurrent.*;
+import com.google.common.util.concurrent.*;
 
 /**
  Skeleton of a ContinuousIntegrationServer which acts as webhook
@@ -21,6 +23,15 @@ import org.eclipse.jgit.api.*;
 */
 public class CI extends AbstractHandler
 {
+    /**
+     * The method called by the github webhook.
+     * @param  target
+     * @param  baseRequest
+     * @param  request
+     * @param  response
+     * @throws IOException
+     * @throws ServletException
+     */
     public void handle(String target,
                        Request baseRequest,
                        HttpServletRequest request,
@@ -30,35 +41,30 @@ public class CI extends AbstractHandler
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
-
         System.out.println(target);
-
-        // here you do all the continuous integration tasks
-        // for example
-        // 1st clone your repository
-        // 2nd compile the code
-
-        gitClone();
-        
-
         response.getWriter().println("CI job done");
-    }
 
-    private static void gitClone() {
-      try{
-      Git git = Git.cloneRepository()
-      .setURI( "https://github.com/filhed97/DD2480-G20-Assignment1.git" )
-      .call();
-      git.checkout().setName( "origin/" + "gradle").call();
-      } catch(Exception e) {
-        System.err.println("Clone exception !!");
-      }
+        Thread t = new Thread(new Runnable(){
+          public void run() {
+             GitUtil.gitClone("assess_error");
+             boolean success = GitUtil.buildRepo();
+             //Link to the email /commit status method here
+          }
+        });
+        t.start();
     }
-
 
     // used to start the CI server in command line
     public static void main(String[] args) throws Exception
     {
+        // Thread t = new Thread(new Runnable(){
+        //   public void run() {
+        //      GitUtil.gitClone("assess_error");
+        //      boolean success = GitUtil.buildRepo();
+        //      //Link the notify and build history here
+        //   }
+        // });
+        // t.start();
 
         Server server = new Server(8020);
         server.setHandler(new CI());
