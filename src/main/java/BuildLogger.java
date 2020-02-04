d240a60e54a54eadfe2f385296aad9c6adfa2366
Susie Lee <susiel2@illinois.edu>
2020-02-04 15:43:46 +0100

    take parsed json objects, add build information to html file #3

--- src/main/java/BuildLogger.java
+++ src/main/java/BuildLogger.java
@@ -4,15 +4,43 @@ import javax.servlet.ServletException;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.*;
-import java.nio.file.attribute.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 
 public class BuildLogger{
 	
-	public void store(HttpServletRequest request){
+	public ParsedData store(HttpServletRequest request){
 		
 		ParsedData payload = new ParsedData(request);
+		return payload;
 		
+	}
+
+	public void createBuildFile(ParsedData buildData) {
+		String buildNumber = "<p> Build Number: " + buildData.id + "<p>";
+		String author = "<p> Author: " + buildData.name + "<p>";
+		String email = "<p> Email: " + buildData.email + "<p>";
+		String timestamp = "<p> Timestamp: " + buildData.timestamp + "<p>";
+		String repo = "<p> Repository: " + buildData.repository + "<p>";
+		String branch = "<p> Branch: " + buildData.branch + "<p>";
+
+		List<String> lines = Arrays.asList(buildNumber, author, email, timestamp, repo, branch);
+
+		File directory = new File("build_history");
+        if (! directory.exists()) {
+			directory.mkdir();
+		}
+
+		Path file = Paths.get("build_history/build" + buildData.id + ".html");
+		try {
+			Files.write(file, lines, StandardCharsets.UTF_8);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
